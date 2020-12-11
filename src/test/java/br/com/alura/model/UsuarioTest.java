d2e8847728f3edd73aa472311dfe87ac454105d9
Rodrigo César Apolináro <rodrigoapolinaro@hotmail.com>
2020-12-11 15:21:27 -0300

    Finalizando e subindo aplicação realizada alura - cusro-java-quarkus-supersonico-e-subatomico

---
+++ src/test/java/br/com/alura/model/UsuarioTest.java
@@ -0,0 +1,29 @@
+package br.com.alura.model;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class UsuarioTest {
+	
+	@Test
+	public void testarSeFindByIdOptionalRetornaUsuarioCorreto() {
+		PanacheMock.mock(Usuario.class);
+		
+		Usuario u = new Usuario();
+		Optional<PanacheEntityBase> usuario = Optional.of(u);
+		
+		Mockito.when(Usuario.findByIdOptional(2)).thenReturn(usuario);
+		
+		Assertions.assertSame(u, Usuario.findByIdOptional(2).get());	
+	}
+
+}
---
+++ src/test/java/br/com/alura/rescource/BitcoinResourceTest.java
@@ -0,0 +1,18 @@
+package br.com.alura.rescource;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+
+@QuarkusTest
+public class BitcoinResourceTest {
+	
+	@Test
+	public void testarSeStatusCodeDaRequisicaoE200() {
+		RestAssured
+			.given().get("bitcoins").then().statusCode(200);
+		
+	}
+
+}
